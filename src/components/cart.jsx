01efbaaba75bef379b2d46cb13a82bba773b5039
Lokesh S <153573033+lokesh-weby@users.noreply.github.com>
2024-08-16 12:44:19 +0530

    added increment and decrement function

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,5 +1,5 @@
 //eslint-disable-next-line
-import React, { useEffect, useState } from "react";
+import React from "react";
 import cartmodule from "../css/cart.module.css";
 import { Link } from "react-router-dom";
 
@@ -9,12 +9,12 @@ const Viewcart = ({
   setcart,
   setLength,
   tot,
-  setTot
+  setTot,
+  qty,
+  setQty
 }) => {
   //removing the matching element by filtering method
-
-  const remove = (id) => {
-    
+  const remove = (id) => { 
     const NewItem = cart.filter((item) => item.id !== id);
     setcart(NewItem);
     setLength(NewItem.length);
@@ -24,11 +24,26 @@ const Viewcart = ({
     cart.reduce((acc, curr) => acc + (isNaN(parseInt(curr.rate * curr.quantity))?0:(parseInt(curr.rate * curr.quantity))), 0)
   );
 
-  const quantityvalue = (id, quantity) => {
+  // const quantityvalue = (id, quantity) => {
+  //   setcart((cart) =>
+  //     cart.map((itm) => (id === itm.id ? { ...itm, quantity: quantity } : itm))
+  //   );
+  
+  // };
+  
+  function decrement(id,originalquantity){
+    
     setcart((cart) =>
-      cart.map((itm) => (id === itm.id ? { ...itm, quantity: quantity } : itm))
+      cart.map((itm) => (id === itm.id ? { ...itm, quantity: originalquantity-1 } : itm))
     );
-  };
+  }
+  function increment(id,originalquantity){
+    setQty(qty+1)
+    setcart((cart) =>
+      cart.map((itm) => (id === itm.id ? { ...itm, quantity: originalquantity+1 } : itm))
+    );
+  }
+
   return (
     <>
       <main className="container">
@@ -36,30 +51,25 @@ const Viewcart = ({
           <h1 className="text-center">
             Your Cart is empty<Link to={"/"}> Order something..</Link>
           </h1>
-        ) : (
-          cart.map((item) => (
-            <div className={cartmodule.items}>
+        ) :
+        (  
+            cart.map((item) => (
+            <div className={cartmodule.items} key={item.id}>
               <div className={cartmodule.cartDetails}>
               <img className={cartmodule.img} src={item.src} alt={item.name} />
                   <h6 className="mx-3 name">{item.name}</h6>
              </div>
               <div className={cartmodule.highlighted}>
                 <div>
-                <span className="rate">{item.rate} x </span> 
-                <input
-                  id="quantity"
-                  value={item.quantity}
-                  onChange={(e) =>
-                    quantityvalue(item.id, parseInt(e.target.value))
-                  }
-                  type="number"
-                  min="0"
-                  max="40"
-                  defaultValue="1"
-                   />
+                {/* input elment */}
+                   <div className= {cartmodule.PlusMinusContainer}>
+                    <button className={cartmodule.controlbtn+" btn"} onClick={()=>{decrement(item.id,(item.quantity<1)?item.quantity=1:item.quantity)}}>-</button>
+                    <p>{item.quantity}</p>
+                    <button className={cartmodule.controlbtn+" btn"}  onClick={()=>{increment(item.id,item.quantity)}}>+</button>
+                   </div>
                 </div>
                 <div>
-                <span className="rate">= {isNaN( item.rate * item.quantity)? 0:item.rate * item.quantity}</span>
+                <span className="rate ">Rs.{isNaN( item.rate * item.quantity)? 0:item.rate * item.quantity}/-</span>
                 </div>
               </div>
               <button
@@ -69,14 +79,16 @@ const Viewcart = ({
               <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
             </svg>
               </button>
+              
             </div>
+            
           ))
         )}
-         {(length<1 || tot===0)?<p></p>: <div className="d-flex justify-content-between my-5">
-          <button className={cartmodule.button+" btn btn-primary"} onClick={()=>{window.print()}}>pay your bill</button>
+ 
+ {(length<1 && tot===0)?<p></p>: <div className="d-flex justify-content-between align-items-center">
+          <button className={cartmodule.button+" btn btn-primary" }onClick={()=>{window.print()}}>pay your bill</button>
           <h1 className="sum">Total: ₹{tot}/-</h1>
           </div>}
-        
         { 
         length < 1 ? <h1>{}</h1> :
             // billing section
@@ -91,22 +103,25 @@ const Viewcart = ({
            </p>
       { 
       cart.map((bills) => (
+        
           (bills.quantity!==0 && !isNaN(bills.quantity))?
            <h4 key={bills.id}>
             <div className="row">
              <p className="col-4">{bills.name}</p>
-             <p className="col-4">{bills.rate+"x"+bills.quantity}</p>
+             <p className="col-4">{bills.quantity}</p>
              <p className="col-4">{bills.rate*bills.quantity}</p>
             </div>
            </h4>
-           :<h1></h1>
+           :<p></p>
        
           ))}
           <div className="row mx-4">
             <p className="col-12 text-end">Total ₹{tot}/-</p>
           </div>
           </div>
+          
            }
+       
       </main>
     </>
   );
